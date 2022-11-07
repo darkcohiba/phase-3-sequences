3784d9c3dbdf9094892f6f9447f7baf53094a101
Sam <samuel.waters@gmail.com>
2022-11-06 17:32:02 -0700

    finished but copilot did it, is that cheating? i think so

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    """Print the first `length` Fibonacci numbers."""
+    seq = []
+    a, b = 0, 1
+    for _ in range(length):
+        seq.append(a)
+        a, b = b, a + b
+    print(seq)
+
+
+print_fibonacci(10)
